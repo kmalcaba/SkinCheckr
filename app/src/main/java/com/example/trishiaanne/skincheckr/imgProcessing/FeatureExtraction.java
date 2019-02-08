f7bcd3d4474d40166119e29574d9df3e57b8ccfa
Kirsten Malcaba <kmalcaba@live.com>
2019-02-08 15:58:01 +0800

    testing GLCM

--- app/src/main/java/com/example/trishiaanne/skincheckr/imgProcessing/FeatureExtraction.java
+++ app/src/main/java/com/example/trishiaanne/skincheckr/imgProcessing/FeatureExtraction.java
@@ -59,44 +59,35 @@ public class FeatureExtraction {
 
     public void extract() {
         calculateGrayValues();
-        normalizeMatrix();
-
-        contrast = calcContrast();
-        homogeneity = calcHomogenity();
-        entropy = calcEntropy();
-        energy = calcEnergy();
-        mean = calcMean();
-        variance = calcVariance();
-        correlation = calcCorrelation();
 
 
 //        this.createMatrix();
-//
-//        //0 angle
-//        int[][] cm0 = createCoOccurrenceMatrix(0);
-//        double[][] cm0Norm = normalizeMatrix(add(cm0, transposeMatrix(cm0)));
-//
-//        //45 angle
-//        int[][] cm45 = createCoOccurrenceMatrix(45);
-//        double[][] cm45Norm = normalizeMatrix(add(cm45, transposeMatrix(cm45)));
-//
-//        //90 angle
-//        int[][] cm90 = createCoOccurrenceMatrix(90);
-//        double[][] cm90Norm = normalizeMatrix(add(cm90, transposeMatrix(cm90)));
-//
-//        //135 angle
-//        int[][] cm135 = createCoOccurrenceMatrix(135);
-//        double[][] cm135Norm = normalizeMatrix(add(cm135, transposeMatrix(cm135)));
-//
-//        //average of the four orientations
-//        this.contrast = (double) (calcContrast(cm0Norm) + calcContrast(cm45Norm) + calcContrast(cm90Norm) + calcContrast(cm135Norm)) / 4;
-//        this.homogeneity = (double) (calcHomogenity(cm0Norm) + calcHomogenity(cm45Norm) + calcHomogenity(cm90Norm) + calcHomogenity(cm135Norm)) / 4;
-//        this.entropy = (double) (calcEntropy(cm0Norm) + calcEntropy(cm45Norm) + calcEntropy(cm90Norm) + calcEntropy(cm135Norm)) / 4;
-//        this.energy = (double) (calcEnergy(cm0Norm) + calcEnergy(cm45Norm) + calcEnergy(cm90Norm) + calcEnergy(cm135Norm)) / 4;
+
+        //0 angle
+        double[][] cm0 = createCoOccurrenceMatrix(0);
+        double[][] cm0Norm = normalizeMatrix(add(cm0, transposeMatrix(cm0)));
+
+        //45 angle
+        double[][] cm45 = createCoOccurrenceMatrix(45);
+        double[][] cm45Norm = normalizeMatrix(add(cm45, transposeMatrix(cm45)));
+
+        //90 angle
+        double[][] cm90 = createCoOccurrenceMatrix(90);
+        double[][] cm90Norm = normalizeMatrix(add(cm90, transposeMatrix(cm90)));
+
+        //135 angle
+        double[][] cm135 = createCoOccurrenceMatrix(135);
+        double[][] cm135Norm = normalizeMatrix(add(cm135, transposeMatrix(cm135)));
+
+        //average of the four orientations
+        this.contrast = (calcContrast(cm0Norm) + calcContrast(cm45Norm) + calcContrast(cm90Norm) + calcContrast(cm135Norm)) / 4;
+        this.homogeneity = (calcHomogenity(cm0Norm) + calcHomogenity(cm45Norm) + calcHomogenity(cm90Norm) + calcHomogenity(cm135Norm)) / 4;
+        this.entropy = (calcEntropy(cm0Norm) + calcEntropy(cm45Norm) + calcEntropy(cm90Norm) + calcEntropy(cm135Norm)) / 4;
+        this.energy = (calcEnergy(cm0Norm) + calcEnergy(cm45Norm) + calcEnergy(cm90Norm) + calcEnergy(cm135Norm)) / 4;
 //        this.dissimilarity = (double) (calcDissimilarity(cm0Norm) + calcDissimilarity(cm45Norm) + calcDissimilarity(cm90Norm) + calcDissimilarity(cm135Norm)) / 4;
-//        this.mean = (double) (calcMean(cm0Norm) + calcMean(cm45Norm) + calcMean(cm90Norm) + calcMean(cm135Norm)) / 4;
-//        this.variance = (double) (calcVariance(cm0Norm) + calcVariance(cm45Norm) + calcVariance(cm90Norm) + calcVariance(cm135Norm)) / 4;
-//        this.correlation = (double) (calcCorrelation(cm0Norm) + calcCorrelation(cm45Norm) + calcCorrelation(cm90Norm) + calcCorrelation(cm135Norm)) / 4;
+        this.mean = (calcMean(cm0Norm) + calcMean(cm45Norm) + calcMean(cm90Norm) + calcMean(cm135Norm)) / 4;
+        this.variance = (calcVariance(cm0Norm) + calcVariance(cm45Norm) + calcVariance(cm90Norm) + calcVariance(cm135Norm)) / 4;
+        this.correlation = (calcCorrelation(cm0Norm) + calcCorrelation(cm45Norm) + calcCorrelation(cm90Norm) + calcCorrelation(cm135Norm)) / 4;
     }
 //
 //    private void createMatrix() {
@@ -117,100 +108,100 @@ public class FeatureExtraction {
 //            }
 //        }
 //    }
-//
-//    //GRAY_LEVEL = 8 bits
-//    //GLCM is created by making a scaled version of the image
-//    //the img is scaled to 8 gray-levels
-//    private int[][] createCoOccurrenceMatrix(int angle) {
-//        //distance = 1
-//        int[][] temp = new int[GRAY_LEVEL + 1][GRAY_LEVEL + 1];
-//        int startRow = 0;
-//        int startColumn = 0;
-//        int endColumn = 0;
-//
-//        boolean isValidAngle = true;
-//
-//        //determining the end column depending on the angle
-//        switch (angle) {
-//            case 0:
-//                startRow = 0;
-//                startColumn = 0;
-//                // ??????????????????
-//                endColumn = grayLevelMatrix[0].length - 2;
-//                break;
-//            case 45:
-//                startRow = 1;
-//                startColumn = 0;
-//                endColumn = grayLevelMatrix[0].length - 2;
-//                break;
-//            case 90:
-//                startRow = 1;
-//                startColumn = 0;
-//                endColumn = grayLevelMatrix[0].length - 1;
-//                break;
-//            case 135:
-//                startRow = 1;
-//                startColumn = 1;
-//                endColumn = grayLevelMatrix[0].length - 1;
-//                break;
-//            default:
-//                isValidAngle = false;
-//                break;
-//        }
-//
-//        // wtf is thissssssssss
-//        if (isValidAngle) {
-//            for (int i = startRow; i < grayLevelMatrix.length; i++) {
-//                for (int j = startColumn; j <= endColumn; j++) {
-//                    switch (angle) {
-//                        case 0:
-//                            temp[grayLevelMatrix[i][j]][grayLevelMatrix[i][j + 1]]++;
-//                            break;
-//                        case 45:
-//                            temp[grayLevelMatrix[i][j]][grayLevelMatrix[i - 1][j + 1]]++;
-//                            break;
-//                        case 90:
-//                            temp[grayLevelMatrix[i][j]][grayLevelMatrix[i - 1][j]]++;
-//                            break;
-//                        case 135:
-//                            temp[grayLevelMatrix[i][j]][grayLevelMatrix[i - 1][j - 1]]++;
-//                            break;
-//                    }
-//                }
-//            }
-//        }
-//
-//        return temp;
-//    }
-//
-//    private double[][] transposeMatrix(double[][] grayLevelMatrix) {
-//        double[][] temp = new double[grayLevelMatrix[0].length][grayLevelMatrix.length];
-//        for (int i = 0; i < grayLevelMatrix.length; i++) {
-//            for (int j = 0; j < grayLevelMatrix[0].length; j++) {
-//                temp[j][i] = grayLevelMatrix[i][j];
-//            }
-//        }
-//
-//        return temp;
-//    }
-//
-//    //add the matrix to its transpose to make it symmetrical
-//    private int[][] add(int[][] m2, int[][] m1) {
-//        int[][] temp = new int[m1[0].length][m1.length];
-//        for (int i = 0; i < m1.length; i++) {
-//            for (int j = 0; j < m1[0].length; j++) {
-//                temp[j][i] = m1[i][j] + m2[i][j];
-//            }
-//        }
-//
-//        return temp;
-//    }
-//
-    private double getTotal() {
-        double temp = 0;
+
+    //GRAY_LEVEL = 8 bits
+    //GLCM is created by making a scaled version of the image
+    //the img is scaled to 8 gray-levels
+    private double[][] createCoOccurrenceMatrix(int angle) {
+        //distance = 1
+        double[][] temp = new double[GRAY_LEVEL + 1][GRAY_LEVEL + 1];
+        int startRow = 0;
+        int startColumn = 0;
+        int endColumn = 0;
+
+        boolean isValidAngle = true;
+
+        //determining the end column depending on the angle
+        switch (angle) {
+            case 0:
+                startRow = 0;
+                startColumn = 0;
+                // ??????????????????
+                endColumn = grayLevelMatrix[0].length - 2;
+                break;
+            case 45:
+                startRow = 1;
+                startColumn = 0;
+                endColumn = grayLevelMatrix[0].length - 2;
+                break;
+            case 90:
+                startRow = 1;
+                startColumn = 0;
+                endColumn = grayLevelMatrix[0].length - 1;
+                break;
+            case 135:
+                startRow = 1;
+                startColumn = 1;
+                endColumn = grayLevelMatrix[0].length - 1;
+                break;
+            default:
+                isValidAngle = false;
+                break;
+        }
+
+        // wtf is thissssssssss
+        if (isValidAngle) {
+            for (int i = startRow; i < grayLevelMatrix.length; i++) {
+                for (int j = startColumn; j <= endColumn; j++) {
+                    switch (angle) {
+                        case 0:
+                            temp[(int) grayLevelMatrix[i][j]][(int)grayLevelMatrix[i][j + 1]]++;
+                            break;
+                        case 45:
+                            temp[(int)grayLevelMatrix[i][j]][(int)grayLevelMatrix[i - 1][j + 1]]++;
+                            break;
+                        case 90:
+                            temp[(int)grayLevelMatrix[i][j]][(int)grayLevelMatrix[i - 1][j]]++;
+                            break;
+                        case 135:
+                            temp[(int)grayLevelMatrix[i][j]][(int)grayLevelMatrix[i - 1][j - 1]]++;
+                            break;
+                    }
+                }
+            }
+        }
+
+        return temp;
+    }
+
+    private double[][] transposeMatrix(double[][] grayLevelMatrix) {
+        double[][] temp = new double[grayLevelMatrix[0].length][grayLevelMatrix.length];
         for (int i = 0; i < grayLevelMatrix.length; i++) {
             for (int j = 0; j < grayLevelMatrix[0].length; j++) {
-                temp += grayLevelMatrix[i][j];
+                temp[j][i] = grayLevelMatrix[i][j];
+            }
+        }
+
+        return temp;
+    }
+
+    //add the matrix to its transpose to make it symmetrical
+    private double[][] add(double[][] m2, double[][] m1) {
+        double[][] temp = new double[m1[0].length][m1.length];
+        for (int i = 0; i < m1.length; i++) {
+            for (int j = 0; j < m1[0].length; j++) {
+                temp[j][i] = m1[i][j] + m2[i][j];
+            }
+        }
+
+        return temp;
+    }
+
+    private double getTotal(double [][] m) {
+        double temp = 0;
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                temp += m[i][j];
             }
         }
 
@@ -218,12 +209,12 @@ public class FeatureExtraction {
     }
 
     //normalizing involves dividing by the sum of values
-    private double[][] normalizeMatrix() {
-        double[][] temp = new double[grayLevelMatrix[0].length][grayLevelMatrix.length];
-        double total = getTotal();
-        for (int i = 0; i < grayLevelMatrix.length; i++) {
-            for (int j = 0; j < grayLevelMatrix[0].length; j++) {
-                temp[j][i] = (double) grayLevelMatrix[i][j] / total;
+    private double[][] normalizeMatrix(double [][] m) {
+        double[][] temp = new double[m[0].length][m.length];
+        double total = getTotal(m);
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                temp[j][i] = (double) m[i][j] / total;
             }
         }
 
@@ -238,44 +229,44 @@ public class FeatureExtraction {
     //multiply each cell by the weight = (x-y)^2
     //x is the cell's row and y is the cell's column
     //add all values
-    private double calcContrast() {
+    private double calcContrast(double[][] m) {
         double temp = 0;
-        for (int i = 0; i < grayLevelMatrix.length; i++) {
-            for (int j = 0; j < grayLevelMatrix[0].length; j++) {
-                temp += grayLevelMatrix[i][j] * Math.pow(i - j, 2);
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                temp += m[i][j] * Math.pow(i - j, 2);
             }
         }
         return temp;
     }
 
     //weights are the inverse of the contrast weight
-    private double calcHomogenity() {
+    private double calcHomogenity(double [][] m) {
         double temp = 0;
-        for (int i = 0; i < grayLevelMatrix.length; i++) {
-            for (int j = 0; j < grayLevelMatrix[0].length; j++) {
-                temp += grayLevelMatrix[i][j] / (1 + Math.pow(i - j, 2));
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                temp += m[i][j] / (1 + Math.pow(i - j, 2));
             }
         }
         return temp;
     }
 
-    private double calcEntropy() {
+    private double calcEntropy(double [][] m) {
         double temp = 0;
-        for (int i = 0; i < grayLevelMatrix.length; i++) {
-            for (int j = 0; j < grayLevelMatrix[0].length; j++) {
-                if (grayLevelMatrix[i][j] != 0) {
-                    temp += (grayLevelMatrix[i][j] * Math.log10(grayLevelMatrix[i][j])) * -1;
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                if (m[i][j] != 0) {
+                    temp += (m[i][j] * Math.log10(m[i][j])) * -1;
                 }
             }
         }
         return temp;
     }
 
-    private double calcEnergy() {
+    private double calcEnergy(double [][] m) {
         double temp = 0;
-        for (int i = 0; i < grayLevelMatrix.length; i++) {
-            for (int j = 0; j < grayLevelMatrix[0].length; j++) {
-                temp += Math.pow(grayLevelMatrix[i][j], 2);
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                temp += Math.pow(m[i][j], 2);
             }
         }
         return temp;
@@ -292,22 +283,22 @@ public class FeatureExtraction {
 //        return temp;
 //    }
 
-    private double calcMean() {
+    private double calcMean(double [][] m) {
         double temp = 0;
-        for (int i = 0; i < grayLevelMatrix.length; i++) {
-            for (int j = 0; j < grayLevelMatrix[0].length; j++) {
-                temp += grayLevelMatrix[i][j] * i;
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                temp += m[i][j] * i;
             }
         }
         return temp;
     }
 
     //standard deviation
-    private double calcVariance() {
+    private double calcVariance(double [][] m) {
         double temp = 0;
-        for (int i = 0; i < grayLevelMatrix.length; i++) {
-            for (int j = 0; j < grayLevelMatrix[0].length; j++) {
-                temp += grayLevelMatrix[i][j] * Math.pow(i - mean, 2);
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                temp += m[i][j] * Math.pow(i - mean, 2);
             }
         }
 
@@ -315,12 +306,12 @@ public class FeatureExtraction {
         return temp;
     }
 
-    private double calcCorrelation() {
+    private double calcCorrelation(double [][] m) {
         double temp = 0;
-        for (int i = 0; i < grayLevelMatrix.length; i++) {
-            for (int j = 0; j < grayLevelMatrix[0].length; j++) {
-                //temp += grayLevelMatrix[i][j] * ( ((i-mean)*(j-mean)) / (Math.sqrt(Math.pow(variance,2))) );
-                temp += grayLevelMatrix[i][j] * (((i - mean) * (j - mean)) / (variance));
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                //temp += m[i][j] * ( ((i-mean)*(j-mean)) / (Math.sqrt(Math.pow(variance,2))) );
+                temp += m[i][j] * (((i - mean) * (j - mean)) / (variance));
             }
         }
         return temp;
