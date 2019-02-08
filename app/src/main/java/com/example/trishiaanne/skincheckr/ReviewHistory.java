98f18065cdaad1e4a2d02a70072e21bae9d9ffd3
Kirsten Malcaba <kmalcaba@gmail.com>
2019-02-09 07:30:17 +0800

    classifier classes

--- app/src/main/java/com/example/trishiaanne/skincheckr/ReviewHistory.java
+++ app/src/main/java/com/example/trishiaanne/skincheckr/ReviewHistory.java
@@ -1,6 +1,7 @@
 package com.example.trishiaanne.skincheckr;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,10 +25,10 @@ public class ReviewHistory extends AppCompatActivity {
     private TextView days;
     private TextView bleed;
 
-    private float [] inputs;
-    private float [][] arrayInput;
+    private float[] inputs;
+    private float[][] arrayInput;
 
-    private ImageClassifier classifier;
+    private Classifier classifier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,19 +72,19 @@ public class ReviewHistory extends AppCompatActivity {
         });
 
         days.setText("Days: " + String.valueOf(daysSymptom));
-        itch.setText("Itching: " + String.valueOf(itching)+ "/10");
-        scale.setText("Scaling/Peeling: " + String.valueOf(scaling)+ "/10");
-        burn.setText("Burning: " + String.valueOf(burning)+ "/10");
-        sweat.setText("Sweating: " + String.valueOf(sweating)+ "/10");
-        crust.setText("Crusting: " + String.valueOf(crusting)+ "/10");
-        if(bleeding == 1){
+        itch.setText("Itching: " + String.valueOf(itching) + "/10");
+        scale.setText("Scaling/Peeling: " + String.valueOf(scaling) + "/10");
+        burn.setText("Burning: " + String.valueOf(burning) + "/10");
+        sweat.setText("Sweating: " + String.valueOf(sweating) + "/10");
+        crust.setText("Crusting: " + String.valueOf(crusting) + "/10");
+        if (bleeding == 1) {
             bleed.setText("Bleeding: " + "Yes");
-        }else{
+        } else {
             bleed.setText("Bleeding: " + "No");
         }
 
         arrayInput = new float[1][14];
-        for(int i = 0; i < inputs.length; i++) {
+        for (int i = 0; i < inputs.length; i++) {
             arrayInput[0][i] = inputs[i];
         }
         arrayInput[0][7] = (float) daysSymptom;
@@ -94,23 +95,19 @@ public class ReviewHistory extends AppCompatActivity {
         arrayInput[0][12] = (float) crusting;
         arrayInput[0][13] = (float) bleeding;
 
-        try {
-            classifier = new ImageClassifier(this);
-            classify();
-            classifier.close();
-        } catch (IOException e) {
-            Log.e("Classifier: ", "Failed to initialize an image classifier.");
-        }
+        classifier = ImageClassifier.create(this, getAssets());
+        classify();
+        classifier.close();
     }
 
     private void classify() {
-        if(classifier==null) {
+        if (classifier == null) {
             Toast.makeText(getApplicationContext(), "Uninitialized classifier.", Toast.LENGTH_LONG).show();
             return;
         }
 
-        float [][] outputs = new float[1][11];
-        outputs = classifier.getOutputs(arrayInput);
-        Log.d("Classifier: ", "Prediction successful");
+        float[][] outputs = new float[1][11];
+//        outputs = classifier.getOutputs(arrayInput);
+//        Log.d("Classifier: ", "Prediction successful");
     }
 }
