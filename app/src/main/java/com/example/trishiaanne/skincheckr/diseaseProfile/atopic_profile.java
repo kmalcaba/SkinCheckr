df0ee6db6e73ac4f8f6f82729b8158fd853a83f3
aarondanielldc <aarondanielldc@gmail.com>
2019-02-15 10:24:54 +0800

    update html

--- app/src/main/java/com/example/trishiaanne/skincheckr/diseaseProfile/atopic_profile.java
+++ app/src/main/java/com/example/trishiaanne/skincheckr/diseaseProfile/atopic_profile.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.widget.TextView;
 
 import com.example.trishiaanne.skincheckr.R;
+
+import org.sufficientlysecure.htmltextview.HtmlHttpImageGetter;
+import org.sufficientlysecure.htmltextview.HtmlTextView;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,7 +18,7 @@ import java.io.InputStream;
 public class atopic_profile extends AppCompatActivity {
     private ViewPager viewPager;
     private int skinDisease = 0;
-    private TextView profile;
+    private HtmlTextView profile;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,9 +31,9 @@ public class atopic_profile extends AppCompatActivity {
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this, skinDisease);
         viewPager.setAdapter(viewPagerAdapter);
 
-        String text = "";
+        String text="";
         try {
-            InputStream inputStream = getAssets().open("atopic.txt");
+            InputStream inputStream = getAssets().open("atopic.html");
             //BufferedReader inputStream = new BufferedReader(new InputStreamReader(getAssets().open("atopic.txt"),"UTF-16LE"));
             int size = inputStream.available();
             byte[] buffer = new byte[size];
@@ -40,6 +44,6 @@ public class atopic_profile extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        profile.setText(text);
+        profile.setHtml(text, new HtmlHttpImageGetter(profile));
     }
 }
